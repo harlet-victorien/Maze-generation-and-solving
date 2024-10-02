c2ff05b8e91cc80a9ddb4370a8427fad57d607ae
vee <joueur441@gmail.com>
2024-10-02 16:57:10 +0200

    clarifying names

--- labyrinthe/everything/palette.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="palette" tilewidth="1" tileheight="1" tilecount="5000" columns="100">
- <image source="palette.png" width="100" height="50"/>
-</tileset>
---
+++ maze/maze generation & solving/archive/murs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="murs" tilewidth="1" tileheight="1" tilecount="856944" columns="1082">
+ <image source="maze---normal-pixel-art-pixel-art-maze-puzzle-path-pixel-8bit-1526876572870.brickImg.jpg" width="1082" height="792"/>
+</tileset>
